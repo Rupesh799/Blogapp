05539438ee44c8716cc05d08c4fea9c04838f39d
Rupesh <rupeshkhatri9816@gmail.com>
2024-07-04 21:58:58 +0545

    fix:blog

--- app/[slug]/page.jsx
+++ app/[slug]/page.jsx
@@ -4,21 +4,22 @@ import Image from 'next/image'
 const SinglePage = () => {
   return (
     <div className={styles.container}>
+         
         <div className={styles.blogHeader}>
-            <div className={styles.imgContainer}>
-                <Image src='/images/tech.jpg' alt=''  className={styles.img}/>
+           <div className={styles.imgContainer}>
+                <Image src='/images/tech.jpg' alt='' fill className={styles.img}/>
             </div>
             <div className={styles.infoContainer}>
                 <div>
-                    <h1>Lorem ipsum dolor sit amet consecte</h1>
+                    <h1 className={styles.title}>Lorem ipsum dolor sit amet consecte</h1>
                 </div>
                 <div className={styles.userInfo}>
                        <div className={styles.userImage}>
-                            <Image src='/images/tech.jpg' alt='' fill/>
+                            <Image src='/images/tech.jpg' alt='' fill className={styles.uimg}/>
                         </div> 
                         <div className={styles.userDetails}>
-                            <h3>John Doe</h3>
-                            <p>07.04.2024</p>
+                            <h3 className={styles.name}>John Doe</h3>
+                            <p className={styles.date}>07.04.2024</p>
                         </div>
                 </div>
             </div>
